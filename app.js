a27b25d70ed3690739cb873b6352c282fa114a40
Aditya Raj Kumawat <arkumawat78@gmail.com>
2021-07-05 20:58:35 +0530

    git add .

--- app.js
+++ app.js
@@ -8,11 +8,11 @@
     drawStuff()
 
     function modifyCaption(caption) {
-        return function updateCaptionDimensions(dimensions) {
+        return function updateCaptionDimensions(_dimensions) {
             caption.x = 0
             caption.y = IMAGE_HEIGHT * 0.7
             caption.w = IMAGE_WIDTH
-            caption.h = dimensions.h
+            caption.h = 30
         }
     }
 
@@ -26,27 +26,66 @@
 
         img.onload = function onImageLoad() {
             let updateCaptionDimensions = modifyCaption(caption)
-            updateCaptionDimensions({ h: IMAGE_HEIGHT })
+            updateCaptionDimensions()
 
             drawImage(img)
 
-            let cap =
-                'I am elmo, and you are watching disney channel, only for kids.'
+            let cap = `hi this is aditya from india but, hi this is aditya from india but, want to get this changed.`
             let capWidth = ctx.measureText(cap).width
 
-            let numberOfLines = capWidth / IMAGE_WIDTH
-            updateCaptionDimensions({ h: 40 * numberOfLines })
+            /**
+             * -> x characters -> y px
+             * => 1 char -> (y/x) px
+             */
+            let widthOfACharacter = capWidth / cap.length
+            console.log(widthOfACharacter) // 4.6645
 
-            ctx.fillStyle = '#00000095'
-            ctx.fillRect(caption.x, caption.y, caption.w, caption.h)
+            let words = cap.split(' ')
+            words = words.map((word) => word + ' ')
+            words.push('$')
 
-            ctx.font = '18px Arial'
-            ctx.fillStyle = '#fff'
-            ctx.fillText(
-                cap,
-                (caption.w - capWidth) / 2,
-                13 + caption.y + (caption.h - 13) / 2,
-            )
+            let lines = []
+
+            let line = ''
+            for (let word of words) {
+                // escape character
+                if (word === '$') {
+                    lines.push(line)
+                    line = word
+                }
+
+                // combine and push
+                if (line.length + word.length <= 35) {
+                    line += word
+                } else {
+                    lines.push(line)
+                    line = word
+                }
+            }
+
+            console.log(lines)
+
+            for (let [idx, line] of lines.entries()) {
+                // console.log('ran')
+                // ctx.fillStyle = '#00000095'
+                // ctx.fillRect(
+                //     caption.x,
+                //     caption.y + 30 * idx,
+                //     caption.w,
+                //     caption.h,
+                // )
+
+                let lineWidth = widthOfACharacter * line.length
+                console.log({ captionWidth: caption.w, lineWidth, line })
+
+                ctx.font = '16px monospace'
+                ctx.fillStyle = '#fff'
+                ctx.fillText(
+                    line,
+                    caption.w - lineWidth,
+                    13 + caption.y + (caption.h - 13) / 2 + 30 * idx,
+                )
+            }
         }
 
         img.src = './selfie.jpg'
