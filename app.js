41ac662117473fca055b44f32f7f491e41834705
Aditya Raj Kumawat <arkumawat78@gmail.com>
2021-07-05 21:18:20 +0530

    i have no idea why i have to hide the first line

--- app.js
+++ app.js
@@ -2,34 +2,11 @@
     let canvas = document.getElementById('canvas')
     let ctx = canvas.getContext('2d')
 
+    const ESCAPE_CHAR = '$'
     const IMAGE_HEIGHT = 821
     const IMAGE_WIDTH = 409
 
     drawStuff()
-
-    var textWidth = (function me(fontSizes, min, max) {
-        var index = Math.floor((min + max) / 2)
-
-        ctx.font = fontSizes[index] + 'px monospace'
-
-        var textWidth = ctx.measureText(
-            'hi this is aditya from india but',
-        ).width
-
-        console.log(textWidth)
-
-        return textWidth
-
-        // if (min > max) {
-        //     return textWidth
-        // }
-
-        // if (textWidth > canvas.width) {
-        //     return me(fontSizes, min, index - 1)
-        // } else {
-        //     return me(fontSizes, index + 1, max)
-        // }
-    })([16], 0, [16].length - 1)
 
     function modifyCaption(caption) {
         return function updateCaptionDimensions(_dimensions) {
@@ -54,7 +31,7 @@
 
             drawImage(img)
 
-            let cap = `lorem ipsum, i have no idea what this means but still I will keep on writing this, I really wanted to get third line and finally I got it`
+            let cap = `lorem ipsum, I really wanted to get third line and finally I got it`
             let capWidth = ctx.measureText(cap).width
 
             /**
@@ -66,13 +43,13 @@
 
             let words = cap.split(' ')
             words = words.map((word) => word + ' ')
-            words.push('$')
+            words.push(ESCAPE_CHAR)
 
             let lines = []
 
             let line = ''
             for (let word of words) {
-                if (word === '$') {
+                if (word === ESCAPE_CHAR) {
                     lines.push(line)
                     line = word
                 }
@@ -84,28 +61,30 @@
                 }
             }
 
-            console.log(lines)
+            lines.unshift(ESCAPE_CHAR)
 
             for (let [idx, line] of lines.entries()) {
-                console.log('ran')
-                ctx.fillStyle = '#00000095'
-                ctx.fillRect(
-                    caption.x,
-                    caption.y + 30 * idx,
-                    caption.w,
-                    caption.h,
-                )
+                if (line !== ESCAPE_CHAR) {
+                    ctx.fillStyle = '#00000095'
+                    ctx.fillRect(
+                        caption.x,
+                        caption.y + 30 * idx,
+                        caption.w,
+                        caption.h,
+                    )
+                }
 
                 let lineWidth = ctx.measureText(line).width
-                console.log({ captionWidth: caption.w, lineWidth, line })
 
                 ctx.font = '16px monospace'
                 ctx.fillStyle = '#fff'
-                ctx.fillText(
-                    line,
-                    (caption.w - lineWidth) / 2,
-                    13 + caption.y + (caption.h - 13) / 2 + 30 * idx,
-                )
+                if (line !== ESCAPE_CHAR) {
+                    ctx.fillText(
+                        line,
+                        (caption.w - lineWidth) / 2,
+                        13 + caption.y + (caption.h - 13) / 2 + 30 * idx,
+                    )
+                }
             }
         }
 
